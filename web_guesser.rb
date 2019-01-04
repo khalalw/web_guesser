a5c73e3ec3ce3b53d1acc5dd49a9377277b57c8d
Khalal Walker <khalal.walker@gmail.com>
2019-01-03 16:09:55 -0800

    add function

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,14 @@ def rand_number(n)
   rand(n)
 end
 
+def check_guess(guess, num)
+  'Too high' if guess.to_i > num
+end
+
 number = rand_number(100)
 
 get '/' do
+  guess = params['guess']
+  message = check_guess(guess)
   erb :index, locals: { number: number }
 end
